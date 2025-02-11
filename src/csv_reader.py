ac27aaea71cb9a035446f50d3dc0148ec144ec88
Alex Schofield <git@ajschof.me>
2025-02-11 23:14:30 +0000

    add type checking to csv_reader function

--- src/csv_reader.py
+++ src/csv_reader.py
@@ -1,7 +1,8 @@
 import csv
 from io import StringIO
+from typing import List, Dict
 
-def csv_reader(file):
-    f = StringIO(file)
+def csv_reader(content: str) -> List[Dict[str, str]]:
+    f = StringIO(content)
     reader = csv.DictReader(f)
     return list(reader)

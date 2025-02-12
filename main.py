a5696fe5939119d091117b0d81976ac1dd708692
Alex Schofield <git@ajschof.me>
2025-02-12 01:49:24 +0000

    add logging & move main.py to project root

---
+++ main.py
@@ -0,0 +1,20 @@
+import csv
+from io import StringIO
+from typing import List, Dict
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+if not logger.handlers:
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+def csv_reader(content: str) -> List[Dict[str, str]]:
+    f = StringIO(content)
+    reader = csv.DictReader(f)
+    logger.info("Finished reading CSV!")
+    return list(reader)
+
+

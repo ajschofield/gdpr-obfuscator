7d9725dcc3270c13fccdb35cbe499ac7a99b87ec
Alex Schofield <git@ajschof.me>
2025-02-17 16:04:09 +0000

    add comments for description of csv_reader.py tests

--- test/test_csv_reader.py
+++ test/test_csv_reader.py
@@ -5,21 +5,24 @@ from obfuscator.csv_reader import CSVReader
 
 reader = CSVReader()
 
-
+# Check if the function can read a CSV string with no content and return
+# an empty list
 def test_empty_csv_should_return_no_content():
     content = ""
     result = reader.read_string(content)
     expected = []
     assert result == expected
 
-
+# Check if the function can read a CSV string with only a header and return
+# an empty list
 def test_csv_with_header_only_should_return_no_content():
     content = "student_id,name,course\n"
     result = reader.read_string(content)
     expected = []
     assert result == expected
 
-
+# Check if the function can read a CSV string with valid data and return
+# a list of dictionaries
 def test_csv_with_valid_data():
     content = (
         "student_id,name,course\n"
@@ -33,7 +36,8 @@ def test_csv_with_valid_data():
     ]
     assert result == expected
 
-
+# Check if the function can read a CSV string with quoted fields and return
+# a list of dictionaries with the quoted fields intact
 def test_csv_with_quoted_fields_should_run_as_expected():
     content = (
         "student_id,name,course\n"

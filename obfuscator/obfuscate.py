792b3589535e747ad25151ed98f3729d1e356aca
Alex Schofield <git@ajschof.me>
2025-02-17 12:37:35 +0000

    create initial function for obfuscation

---
+++ obfuscator/obfuscate.py
@@ -0,0 +1,7 @@
+from typing import List, Dict
+from obfuscator.logger import get_logger
+
+logger = get_logger("Obfuscator")
+
+def obfuscate(data: List[Dict[str, str]], pii_fields: List[str]) -> List[Dict[str,str]]:
+    pass

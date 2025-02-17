97b345545b950615ebd1df77fd0c1a8a4300495a
Alex Schofield <git@ajschof.me>
2025-02-17 12:50:36 +0000

    use list/dictionary comprehension to obfuscate pii data

--- obfuscator/obfuscate.py
+++ obfuscator/obfuscate.py
@@ -4,4 +4,12 @@ from obfuscator.logger import get_logger
 logger = get_logger("Obfuscator")
 
 def obfuscate(data: List[Dict[str, str]], pii_fields: List[str]) -> List[Dict[str,str]]:
-    pass
+    if not data:
+        logger.info("No valid data was provided to obfuscate")
+        return []
+    
+    return [
+        {k: ("***" if k in pii_fields else v) for k, v in record.items()}
+        for record in data
+    ]
+    

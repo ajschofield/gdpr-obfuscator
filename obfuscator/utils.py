d0a8826f43a48e3db53f4ec3f62d5b6e5f3fd15d
Alex Schofield <git@ajschof.me>
2025-02-18 22:11:01 +0000

    add logging to utils.py

--- obfuscator/utils.py
+++ obfuscator/utils.py
@@ -1,8 +1,15 @@
 # Utility functions
+from obfuscator.logger import get_logger
+
+# Create the logger
+logger = get_logger("CLI")
 
 
 def get_s3_path(uri):
     parts = uri.replace("s3://", "").split("/")
+    logger.debug(f"Parts: {parts}")
     bucket = parts.pop(0)
+    logger.debug(f"Bucket: {bucket}")
     key = "/".join(parts)
+    logger.debug(f"Key: {key}")
     return bucket, key

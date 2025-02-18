fb2488ebb6c0b48ff1d24bcb14d4301dc7f006ea
Alex Schofield <git@ajschof.me>
2025-02-18 23:27:30 +0000

    add function to return byte-stream of obfuscated csv file

---
+++ obfuscator/csv_writer.py
@@ -0,0 +1,26 @@
+import csv
+import io
+from typing import List, Dict
+from obfuscator.logger import get_logger
+
+# Create the logger
+logger = get_logger("CSVWriter")
+
+
+def data_to_csv_bytes(data: List[Dict[str, str]]) -> bytes:
+    if not data:
+        logger.info("No valid data was provided to write")
+        return b""
+
+    output = io.StringIO()
+
+    headers = list(data[0].keys())
+
+    writer = csv.DictWriter(output, fieldnames=headers)
+    writer.writeheader()
+    writer.writerows(data)
+
+    csv_string = output.getvalue()
+    logger.debug(f"CSV data: {csv_string}")
+
+    return csv_string.encode("utf-8")

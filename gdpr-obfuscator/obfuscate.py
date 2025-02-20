f8e6f1be79a4d6b5262cc3edaad1512cd26337ee
Alex Schofield <git@ajschof.me>
2025-02-20 18:18:46 +0000

    update author variable in pyproject.toml to fix build issue

---
+++ gdpr-obfuscator/obfuscate.py
@@ -0,0 +1,26 @@
+from typing import List, Dict
+from obfuscator.logger import get_logger
+
+logger = get_logger("OBFUSCATE")
+
+
+def obfuscate(
+    data: List[Dict[str, str]], pii_fields: List[str]
+) -> List[Dict[str, str]]:
+    """
+    A function to obfuscate PII fields in a list of dictionaries, replacing
+    sensitive values with a string of asterisks.
+    """
+    if not data:
+        logger.error(
+            "Invalid or empty data was provided to obfuscate. Returning empty list."
+        )
+        return []
+    if not pii_fields:
+        logger.error("No PII fields provided to obfuscate. Returning data unchanged.")
+        return data
+
+    return [
+        {k: ("***" if k in pii_fields else v) for k, v in record.items()}
+        for record in data
+    ]

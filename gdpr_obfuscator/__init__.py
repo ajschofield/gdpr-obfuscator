4cfffdcceec64b8eeb6bd861214c82aed8a51d2c
Alex Schofield <git@ajschof.me>
2025-02-21 02:39:09 +0000

    create byte stream within main process_s3 & process_local methods

--- gdpr_obfuscator/__init__.py
+++ gdpr_obfuscator/__init__.py
@@ -1,14 +1,18 @@
 from .read import DataReader
 from .obfuscate import obfuscate_data
+from .utils import Utilities
 from typing import List, Dict
 
 
 class Obfuscator:
     def __init__(self):
         self.reader = DataReader()
+        self.utils = Utilities()
 
     def process_s3(self, path: str, pii_fields: List[str]) -> bytes:
-        return obfuscate_data(self.reader.read_s3(path), pii_fields)
+        obfuscated_data = obfuscate_data(self.reader.read_s3(path), pii_fields)
+        return self.utils.create_byte_stream(obfuscated_data)
 
     def process_local(self, path: str, pii_fields: List[str]) -> bytes:
-        return obfuscate_data(self.reader.read_local(path), pii_fields)
+        obfuscated_data = obfuscate_data(self.reader.read_local(path), pii_fields)
+        return self.utils.create_byte_stream(obfuscated_data)

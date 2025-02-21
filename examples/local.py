ad6a1e55f7b4d558d61c8d937815d461f0172de2
Alex Schofield <git@ajschof.me>
2025-02-21 02:49:46 +0000

    print result in examples/local.py for testing/demo

--- examples/local.py
+++ examples/local.py
@@ -3,9 +3,4 @@ from gdpr_obfuscator import Obfuscator
 obfuscator = Obfuscator()
 
 
-def main():
-    print(obfuscator.process_local(path="mock_data.csv", pii_fields=["name", "email"]))
-
-
-if __name__ == "__main__":
-    main()
+print(obfuscator.process_local(path="mock_data.csv", pii_fields=["name", "email"]))

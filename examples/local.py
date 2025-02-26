c2a762d71b67ee957a2e7730bed336411f65d1d6
Alex Schofield <git@ajschof.me>
2025-02-26 16:28:38 +0000

    use larger dataset when testing examples/local.py

--- examples/local.py
+++ examples/local.py
@@ -2,8 +2,6 @@ from gdpr_obfuscator import Obfuscator
 
 obfuscator = Obfuscator()
 
-json_input = (
-    '{"file_path": "./test/data/mock_data.csv", "pii_fields": ["name", "email"]}'
-)
+json_input = '{"file_path": "./test/data/large_dataset.csv", "pii_fields": ["full_name", "email_address"]}'
 
 print(obfuscator.process_local(json_input))
